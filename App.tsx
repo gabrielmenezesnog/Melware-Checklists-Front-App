4c60abba0bd509fc3cf77ff15734533fafa8f01b
gabrielmenezesn <gabriel.menezes@grupoirrah.com>
2023-11-18 22:47:55 -0300

    commit inicial

---
+++ App.tsx
@@ -0,0 +1,20 @@
+// Gesture handler
+// import 'react-native-gesture-handler';
+import React from 'react';
+import {Text} from 'react-native';
+// import {Platform, StatusBar} from 'react-native';
+// import {Routes} from './src/navigation';
+
+export const App = () => {
+  return (
+    <>
+      {/* {Platform.OS === 'android' && (
+        <StatusBar translucent={true} backgroundColor="rgba(0, 0, 0, 0.0)" />
+      )}
+      <Routes /> */}
+      <Text>Teste</Text>
+    </>
+  );
+};
+
+export default App;
